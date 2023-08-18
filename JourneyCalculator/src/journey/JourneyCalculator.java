fa33e4309c97989a4ccb21e44fe8b0b78d115cbf
harshame <harshame74@gmail.com>
2023-08-18 20:04:14 +0530

    PROGRAMS ON METHODS
    ALL PROGRAMS BELONGS TO CONCEPT CALLED METHODS

---
+++ JourneyCalculator/src/journey/JourneyCalculator.java
@@ -0,0 +1,12 @@
+package journey;
+
+public class JourneyCalculator {
+	public double calculateDistance(double speed, double time)
+
+	{
+
+	return speed*time;
+
+	}
+
+}
---
+++ String/src/StringJoiner/stringapp.java
@@ -0,0 +1,11 @@
+package StringJoiner;
+
+public class stringapp {
+	public static String joinStrings(String str1, String str2)
+
+	{
+
+	return str1+str2;
+
+	}
+}
